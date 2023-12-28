e105fdc24f4526114d84208354afe5c49d6550a3
Flynn Dreilinger <flynnd@stanford.edu>
2023-12-28 12:02:08 -0800

    only CI

--- src/function_a.jl
+++ src/function_a.jl
@@ -1,8 +1,33 @@
+
 """
     sum = function_a(a, b)
     sum == a + b
 
 Return the sum of `a` and `b`.
+
+It can render math too
+
+```math
+x_{1,2} = \\frac{a}{b}
+```
+
+!!! warning
+    This function is not optimized for speed.
+
+!!! note
+    This function is slow
+
+!!! tip
+    This function is not fast
+
+!!! example
+    ```julia
+    julia> function_a(1, 2)
+    3
+    ```
+
+
+
 """
 function function_a(a, b)
     return a + b
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,4 @@ using Test
 
 @testset "SkyGraphTest.jl" begin
     @test function_a(1, 2) == 3
-    @test function_a(1, 2) == 4
 end
